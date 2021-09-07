c7952d829488dab17e8093a42bd2be64c478f339
KimJoongHyeok <moon960427@gmail.com>
2021-09-07 22:55:40 +0900

    05day_JDBC_계좌생성예외처리

---
+++ Database/07-jdbc-bank-project/src/test/CreateAccountException.java
@@ -0,0 +1,12 @@
+package test;
+
+public class CreateAccountException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5092749749342331232L;
+
+	public CreateAccountException(String message) {
+		super(message);
+	}
+}
