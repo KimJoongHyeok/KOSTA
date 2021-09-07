fe0906dc2ce9d5db23d2e2947e56c34f7636b46f
KimJoongHyeok <moon960427@gmail.com>
2021-09-07 22:55:22 +0900

    05day_JDBC_미등록계좌예외처리

---
+++ Database/07-jdbc-bank-project/src/test/AccountNotFoundException.java
@@ -0,0 +1,14 @@
+package test;
+
+public class AccountNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8940610579298093343L;
+
+	public AccountNotFoundException(String message) {
+		super(message);
+	}
+
+}
