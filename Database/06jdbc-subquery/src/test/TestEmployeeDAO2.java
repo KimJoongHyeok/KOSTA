0815b38328c0d69a1605270c1f8e07418410f1bf
KimJoongHyeok <moon960427@gmail.com>
2021-09-07 22:54:17 +0900

    05day_JDBC_TestEmployee

---
+++ Database/06jdbc-subquery/src/test/TestEmployeeDAO2.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.Employee;
+import model.EmployeeDAO;
+
+public class TestEmployeeDAO2 {
+	public static void main(String[] args) {
+		try {
+			EmployeeDAO dao = new EmployeeDAO();
+			String job = "개발";
+			/*
+			 * 해당 job 의 사원중 가장 높은 salary 를 받는 사원정보를 조회하는 메서드
+			 */
+			ArrayList<Employee>list = dao.getEmployeeListByHighSalAndJob(job);
+			for(int i = 0;i<list.size();i++) {
+				System.out.println(list.get(i));
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
