40266ffbe55a4ff917cfb2b908298c9f901aa4a4
KimJoongHyeok <moon960427@gmail.com>
2021-09-08 21:47:07 +0900

    06day_JDBC

---
+++ Database/07-jdbc-bank-project/src/test/TestUnit44.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestUnit44 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Database/07-jdbc-bank-project/src/test/TestUnit7.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.sql.SQLException;
+
+import model.AccountDAO;
+
+//step7. 계좌이체 테스트
+public class TestUnit7 {
+	public static void main(String[] args) {
+		try {
+			AccountDAO dao = new AccountDAO();
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 	
+	}
+}
