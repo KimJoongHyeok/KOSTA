f4c1f2f650ca2bd3e3079d7fcc0816d542e78f41
KimJoongHyeok <moon960427@gmail.com>
2021-09-08 21:45:46 +0900

    06day_JDBC_금액0원이하 예외처리

---
+++ Database/07-jdbc-bank-project/src/model/NotMoneyException.java
@@ -0,0 +1,11 @@
+package model;
+
+public class NotMoneyException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6470615568872477633L;
+	public NotMoneyException(String message){
+		super(message);
+	}
+}
