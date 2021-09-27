6de89a83ae53fe9ccb251114a084d0e4f507766e
KimJoongHyeok <moon960427@gmail.com>
2021-09-27 20:48:56 +0900

    06day_Web_contextDestroyed,contextInitialiezed확인 예제

---
+++ Web/webstudy08-ServletConfig-ServletContext/src/main/java/step3/TestListener.java
@@ -0,0 +1,34 @@
+package step3;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Application Lifecycle Listener implementation class TestListener
+ *
+ */
+public class TestListener implements ServletContextListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public TestListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * web Application 종료 직전에 호출
+	 */
+	public void contextDestroyed(ServletContextEvent arg0) {
+		System.out.println("conetextDestroyed");
+	}
+
+	/**
+	 * web application 시작 시점에 호출
+	 */
+	public void contextInitialized(ServletContextEvent event) {
+		String security = event.getServletContext().getInitParameter("appSecurity");
+		System.out.println("contextInitialized security info : " + security);
+	}
+
+}
