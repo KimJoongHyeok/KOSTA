9516e3b2cf1dbf2b1d368f248ea5d4debb6d5603
KimJoongHyeok <moon960427@gmail.com>
2021-10-08 10:03:01 +0900

    12day_Web_회원관리미니프로젝트_MemberDAO

---
+++ Web/webstudy18-member-usecase-diagram/src/main/java/org/kosta/webstudy18/model/MemberDAO.java
@@ -0,0 +1,155 @@
+package org.kosta.webstudy18.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/*
+ * Singleton Design Pattern 을 적용 : 시스템 상에서 단 한번 객체를 생성해 공유하여 사용하고자 할 때 적용하는 설계 패턴
+ */
+public class MemberDAO {
+	private static MemberDAO instance = new MemberDAO();
+	private String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	private String username = "scott";
+	private String userpass = "tiger";
+
+	private MemberDAO() {
+	}
+
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+
+	// closeAll 오버로딩 2개 만든다.
+	public void closeAll(PreparedStatement pstmt, Connection con) throws SQLException {
+		if (pstmt != null)
+			pstmt.close();
+		if (con != null)
+			con.close();
+	}
+
+	public void closeAll(ResultSet rs, PreparedStatement pstmt, Connection con) throws SQLException {
+		if (rs != null)
+			rs.close();
+		closeAll(pstmt, con);
+	}
+	
+	public MemberVO findMemberById(String id) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		MemberVO vo = null;
+		try {
+			con = DriverManager.getConnection(url, username, userpass);
+			String sql = "select id,name,address from member where id = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				vo = new MemberVO(rs.getString(1),rs.getString(2),rs.getString(3));
+			}
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return vo;
+	}
+	public MemberVO login(String id,String password) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		MemberVO vo = null;
+		try {
+			con = DriverManager.getConnection(url, username, userpass);
+			String sql = "select id,password,name,address from member where id = ? and password = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, password);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				vo = new MemberVO(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4));
+			}
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return vo;
+	}
+	
+	public ArrayList<MemberVO> findMemberListByAddress(String address) throws SQLException {
+		MemberVO vo = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			con = DriverManager.getConnection(url, username, userpass);
+			String sql = "select id,name,address from member where address = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, address);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				list.add(new MemberVO(rs.getString(1),rs.getString(2),rs.getString(3)));
+			}
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return list;
+	}
+	public void updateMember(MemberVO vo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = DriverManager.getConnection(url, username, userpass);
+			String sql = "update member set password = ? , name = ?, address = ? where id = ?;";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getPassword());
+			pstmt.setString(2, vo.getName());
+			pstmt.setString(3, vo.getAddress());
+			pstmt.setString(4, vo.getId());
+			pstmt.executeUpdate();
+		}finally {
+			
+		}
+	}
+	public void registerMember(MemberVO vo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = DriverManager.getConnection(url, username, userpass);
+			String sql = "insert into member values(?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getName());
+			pstmt.setString(4, vo.getAddress());
+			pstmt.executeUpdate();
+			
+		}
+		finally {
+			closeAll(pstmt, con);
+		}
+	}
+	public boolean idCheck(String id) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean flag = false;
+		//true 면 아이디 있는거 false 면 아이디 없는거
+		try {
+			con = DriverManager.getConnection(url, username, userpass);
+			String sql = "select * from member where id = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				flag = true;
+			}
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return flag;
+	}
+}
