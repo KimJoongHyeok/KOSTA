c7384dacb2b2135f3ed0b38cfce7976d45ab9771
KimJoongHyeok <moon960427@gmail.com>
2021-10-18 18:32:51 +0900

    17day_Web_FrontController_*.do

---
+++ web-workspace2/webstudy23-date/src/main/java/org/kosta/webstudy23/controller/FrontControllerServlet.java
@@ -0,0 +1,50 @@
+package org.kosta.webstudy23.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * .do 스타일 요청을 모두 처리하기 위해 url-pattern 설정을 *.do 로 할당
+ */
+@WebServlet("*.do")
+public class FrontControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doDispatch(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String uri = request.getRequestURI();
+			String contextPath = request.getContextPath();
+			String command = uri.substring(contextPath.length()+1,uri.length()-3);
+			//System.out.println(command);
+			Controller controller = HandlerMapping.getInstance().create(command);
+			String path = controller.execute(request, response);
+			if(path.startsWith("redirect:")) {
+				response.sendRedirect(path);
+			}else {
+				request.getRequestDispatcher(path).forward(request, response);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("error.jsp");
+		}
+	
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doDispatch(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		doDispatch(request, response);
+	}
+
+}
