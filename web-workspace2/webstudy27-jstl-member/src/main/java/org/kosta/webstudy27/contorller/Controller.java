b51284e507c27936815a9a10fdc7c6b04a73cde3
KimJoongHyeok <moon960427@gmail.com>
2021-10-22 21:57:05 +0900

    no message

--- web-workspace2/webstudy27-jstl-member/src/main/java/org/kosta/webstudy27/contorller/Controller.java
+++
@@ -1,8 +0,0 @@
-package org.kosta.webstudy27.contorller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Controller {
-	public String execute(HttpServletResponse response,HttpServletRequest request) throws Exception;
-}
