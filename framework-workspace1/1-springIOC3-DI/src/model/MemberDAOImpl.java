fee942c443fd77f3f37e28a6a30b9b93a29d3aae
KimJoongHyeok <moon960427@gmail.com>
2021-11-18 18:59:16 +0900

    02day_SpringIOC_Maven

---
+++ framework-workspace1/1-springIOC3-DI/src/model/MemberDAOImpl.java
@@ -0,0 +1,12 @@
+package model;
+
+public class MemberDAOImpl implements MemberDAO {
+	@Override
+	public String findMemberById(String id) {
+		if(id.equals("java")) {
+			return "java 아이유 오리";
+		}else {
+			return null;
+		}
+	}
+}
