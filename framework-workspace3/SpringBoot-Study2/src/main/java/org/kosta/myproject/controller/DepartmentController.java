3b7aac5e684a41257f33cbc7133ee27e8197009e
KimJoongHyeok <moon960427@gmail.com>
2021-12-07 23:23:06 +0900

    13day_SpringBoot_Controller

---
+++ framework-workspace3/SpringBoot-Study2/src/main/java/org/kosta/myproject/controller/DepartmentController.java
@@ -0,0 +1,24 @@
+package org.kosta.myproject.controller;
+
+import org.kosta.myproject.model.mapper.DepartmentMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DepartmentController {
+	
+	private DepartmentMapper departmentMapper;
+	
+	public DepartmentController(DepartmentMapper departmentMapper) {
+		super();
+		this.departmentMapper = departmentMapper;
+	}
+
+	@RequestMapping("getTotalDepartmentCount")
+	public String getTotalDepartmentCount(Model model) {
+		
+		model.addAttribute("totalDeptCount",departmentMapper.getTotalDepartmentCount());
+		return "department/dept-count";
+	}
+}
