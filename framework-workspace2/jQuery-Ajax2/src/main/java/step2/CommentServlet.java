e8c17f9929b1d5a19fdb1406eb93d5819787e19b
KimJoongHyeok <moon960427@gmail.com>
2021-12-08 22:52:47 +0900

    14day_jQuery_Ajax

--- framework-workspace2/jQuery-Ajax2/src/main/java/step2/CommentServlet.java
+++ framework-workspace2/jQuery-Ajax2/src/main/java/step2/CommentServlet.java
@@ -14,7 +14,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/CommentServlet")
 public class CommentServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
